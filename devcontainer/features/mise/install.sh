cd11ceab83d44947dce9dc15b32ae65e293fa5a0
shotor <shotor@shotor.com>
2026-08-10 23:07:17 +0200

    fix(mise): don't use sudo

--- devcontainer/features/mise/install.sh
+++ devcontainer/features/mise/install.sh
@@ -5,11 +5,12 @@ USER="${_REMOTE_USER}"
 HOME="$(getent passwd "$USER" | cut -d: -f6)"
 ZSHRC="$HOME/.zshrc"
 
-sudo -u "$USER" \
-  HOME="$HOME" \
-  sh -c 'curl https://mise.run | sh'
+su -s /bin/sh "$USER" -c \
+  "HOME='$HOME' curl -fsSL https://mise.run | sh"
 
 MISE_INIT='eval "$(~/.local/bin/mise activate zsh)"'
+
+touch "$ZSHRC"
 
 grep -Fqx "$MISE_INIT" "$ZSHRC" 2>/dev/null \
   || echo "$MISE_INIT" >> "$ZSHRC"
