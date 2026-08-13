27f01e99318aac6d304cbf3263a8f1a1b7230f9f
shotor <shotor@shotor.com>
2026-08-13 19:51:27 +0200

    feat(go): add feature

---
+++ devcontainer/features/go/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+USER="${_REMOTE_USER}"
+HOME="$(getent passwd "$USER" | cut -d: -f6)"
+
+su -s /bin/sh "$USER" -c \
+  "HOME='$HOME' '$HOME/.local/bin/mise' use -g go@1.26"
