1288a29a44f63d512b384795a869811d9cf6774c
shotor <shotor@shotor.com>
2026-08-24 15:29:05 +0200

    feat(node): symlink node shims

--- devcontainer/features/node/install.sh
+++ devcontainer/features/node/install.sh
@@ -6,3 +6,12 @@ HOME="$(getent passwd "$USER" | cut -d: -f6)"
 
 su -s /bin/sh "$USER" -c \
   "HOME='$HOME' '$HOME/.local/bin/mise' use -g node@24"
+
+NODE_DIR="$(
+  su -s /bin/sh "$USER" -c \
+    "export HOME='$HOME'; '$HOME/.local/bin/mise' where node"
+)"
+
+ln -sf "$NODE_DIR/bin/node" /usr/local/bin/node
+ln -sf "$NODE_DIR/bin/npm" /usr/local/bin/npm
+ln -sf "$NODE_DIR/bin/npx" /usr/local/bin/npx
