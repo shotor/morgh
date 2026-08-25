5a4c01554cf0015a3f66e2226e75e1a9821119c4
shotor <shotor@shotor.com>
2026-08-25 16:02:29 +0200

    fix(dotfiles): aliases

--- dotfiles/zsh/.oh-my-zsh/custom/aliases.zsh
+++ dotfiles/zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,13 +1,14 @@
 alias ls='lsd'
+
 alias DOITNOW='gaa && gcn! && gpf'
+
 if ! command -v code >/dev/null 2>&1; then
   alias code='vscodium'
 fi
 
 c() {
   {
-    printf '$'
-    printf ' %q' "$@"
+    printf '%q' "$@"
     printf '\n'
     "$@" 2>&1
   } | tee >(wl-copy)
