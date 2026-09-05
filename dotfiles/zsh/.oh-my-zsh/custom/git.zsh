34776916beac5939ca0e607bfed72af79ba3a6f7
shotor <shotor@shotor.com>
2026-09-05 16:06:06 +0200

    fix(dotfiles): git signing key path

--- dotfiles/zsh/.oh-my-zsh/custom/git.zsh
+++ dotfiles/zsh/.oh-my-zsh/custom/git.zsh
@@ -2,7 +2,9 @@ gitshotor() {
   git config user.name "shotor"
   git config user.email "shotor@shotor.com"
   git config gpg.format ssh
-  git config user.signingkey ~/.ssh/shotor_id_ed25519.pub
+  # quoted: git expands ~ itself, the shell's expansion writes this machine's home
+  # into the checkout, which devvm pull/sync carries to the other one
+  git config user.signingkey '~/.ssh/shotor_id_ed25519.pub'
   git config commit.gpgsign true
   git config tag.gpgSign true
 
